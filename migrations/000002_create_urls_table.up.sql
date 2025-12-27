7c5ed8f6d80011d348649dff7ca67132791ebd41
Maxim <coolycow.com@gmail.com>
2025-12-27 19:43:10 +0300

    fix: изменена схема миграций, т.к. не проходит тест Инкремента 10

--- migrations/000002_create_urls_table.up.sql
+++ migrations/000002_create_urls_table.up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE urls (
                       id SERIAL PRIMARY KEY,
-                      user_id INTEGER NULL,
+                      user_id INTEGER NOT NULL,
                       url TEXT NOT NULL,
-                      key VARCHAR(255) NOT NULL,
-                      CONSTRAINT fk_urls_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+                      key VARCHAR(255) NOT NULL
 );
 
 CREATE INDEX idx_urls_user_id ON urls(user_id);
