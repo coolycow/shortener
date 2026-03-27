43ca8187d18e46a39001539d24a1d343baaff50a
Maxim <coolycow.com@gmail.com>
2026-03-27 14:58:31 +0300

    Выполнено задание по Инкременту 26

--- internal/repository/json_storage.go
+++ internal/repository/json_storage.go
@@ -9,16 +9,31 @@ import (
 	"github.com/google/uuid"
 )
 
+// JSONStorage представляет хранилище данных в формате JSON
 type JSONStorage struct {
 	file    *os.File
 	encoder *json.Encoder
 	decoder *json.Decoder
 }
 
+// Close закрывает файл
 func (s *JSONStorage) Close() error {
+	// Если файл не открыт, то просто возвращаем nil
+	if s.file == nil {
+		return nil
+	}
+
+	// Синхронизируем данные с диском
+	if err := s.file.Sync(); err != nil {
+		_ = s.file.Close()
+		return err
+	}
+
+	// Закрываем файл
 	return s.file.Close()
 }
 
+// Load загружает данные из файла
 func (s *JSONStorage) Load(repo URLRepository) error {
 	for s.decoder.More() {
 		var url model.ShortURL
@@ -35,6 +50,7 @@ func (s *JSONStorage) Load(repo URLRepository) error {
 	return nil
 }
 
+// Write записывает данные в файл
 func (s *JSONStorage) Write(url model.ShortURL) error {
 	err := s.encoder.Encode(url)
 
@@ -45,6 +61,7 @@ func (s *JSONStorage) Write(url model.ShortURL) error {
 	return nil
 }
 
+// NewJSONStorage создает новый экземпляр JSONStorage
 func NewJSONStorage(filename string) (*JSONStorage, error) {
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
